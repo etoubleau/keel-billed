0fcd84216798f6befe9d4698dcfe7f3decfeeffa
Margot <margot.sylvain@gmail.com>
2017-02-09 17:39:43 +0100

    report et dashboard

--- hospiboard/preprocess/augment.py
+++ hospiboard/preprocess/augment.py
@@ -10,4 +10,8 @@ def augment(dfs):
     Insert here code to augment your data frames during preprocessing
     """
 
+    # Add domain to each dataframe
+    for domain, df in dfs.iteritems():
+        df['domain'] = domain
+
     return dfs
